904c09f0bb92407e9f9384fc251b6aec31e8718c
ravikiranyenala <yravikiranece@gmail.com>
2020-09-02 02:33:39 +0530

    played with javaScript basic functions

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+
+
+const myText = document.querySelector("h1");
+myText.textContent="Hello world....!"
+
+let bike = "sports";
+/*document.querySelector("button").onclick=function(){
+	alert("yes your entering into the js file now");
+	setUserName()
+
+}
+
+*/
+function setUserName(){
+	let myName = prompt("Enter your name: ");
+	if(!myName){
+		setUserName()
+	}
+	else{
+
+	localStorage.setItem('name',myName);
+	myText.textContent = "Mozilla is cool "+myName;
+
+}
+}
+
+let myImage = document.querySelector('img');
+myImage.onclick=function(){
+	let mySrc = myImage.getAttributes('src');
+	prompt("welcome to image change");
+	if(mySrc==='images/Mozilla-firefox.jpg'){
+		myImage.setAttributes('mySrc','images/Mozilla-firefox2.jpg');
+	}
+	else{
+		myImage.setAttributes('mySrc','images/Mozilla-firefox');
+
+	}
+}
